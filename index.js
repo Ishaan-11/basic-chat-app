b541c039eb509a0597d4c2520b3628b75cf4792b
Ishaan-11 <ishaansharma1998@gmail.com>
2021-05-15 15:53:20 +0530

    add: emitting events

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const http = require('http');
-
 const app = express();
-app.use(express.static("public"));
 const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+app.use(express.static("public"));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
+});
+
+io.on('connection', socket => {
+  console.log('a user connected');
+
+  socket.on('chat message', msg => {
+    console.log('message: ' + msg);
+  })
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 server.listen(3000, () => {

e1d5a4910b7e597a4d5e1776f5b0afb9dfdc300c
Ishaan-11 <ishaansharma1998@gmail.com>
2021-05-16 19:29:48 +0530

    implement jQuery

--- public/main.js
+++ public/main.js
@@ -1,21 +1,21 @@
 const socket = io();
 
-const messages = document.getElementById('messages');
-const form = document.getElementById('form');
-const input = document.getElementById('input');
+const messages = $('#messages');
+const form = $('#form');
+const input = $('#input');
 
-form.addEventListener('submit', e => {
+form.on('submit', e => {
   e.preventDefault();
-  if(input.value) {
-    socket.emit('chat message', input.value);
-    input.value = '';
+  if(input.val()) {
+    socket.emit('chat message', input.val());
+    input.val('');
     input.focus();
   }
 });
 
 socket.on('chat message', msg => {
-  const item = document.createElement('li');
-  item.textContent = msg;
+  const item = $('<li></li>');
+  item.text(msg);
   addMesageElement(item);
 });
 
@@ -30,13 +30,13 @@ socket.on('user joined', msg => {
 
 
 function log(msg) {
-  const item = document.createElement('li');
-  item.textContent = msg;
-  item.className= 'log';
+  const item = $('<li></li>');
+  item.text(msg);
+  item.addClass('log');
   addMesageElement(item);
 }
 
 function addMesageElement(ele) {
-  messages.appendChild(ele);
+  messages.append(ele);
   window.scrollTo(0, document.body.scrollHeight);
 }
